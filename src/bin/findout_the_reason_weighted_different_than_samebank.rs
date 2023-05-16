292b32a6bcd1a6e3f27484040a5483d6442c1d34
Jiangqiu Shen <ivy22233qiu@live.com>
2023-05-16 15:30:35 -0400

    update the bin

--- src/bin/findout_the_reason_weighted_different_than_samebank.rs
+++ src/bin/findout_the_reason_weighted_different_than_samebank.rs
@@ -1,13 +1,11 @@
-#![allow(unused, clippy::too_many_arguments)]
-use std::{collections::BTreeMap, path::PathBuf};
+#![allow(clippy::too_many_arguments)]
+use std::path::PathBuf;
 
 use clap::Parser;
-use itertools::{izip, Itertools};
 use spmspm_pim::{
     analysis::remap_analyze::{
         jump::{MyJumpOpt, NormalJumpCycle},
         real_jump::RealJumpResult,
-        row_cycle::*,
     },
     pim::configv2::MappingType,
     tools::file_server,
@@ -46,7 +44,7 @@ fn main() -> eyre::Result<()> {
         |_g, m| {
             println!("map_end: {:?}\n\n", m);
         },
-        |graph_name, mapping_type, single_result| {
+        |_graph_name, _mapping_type, single_result| {
             let remote_write = single_result.remote_dense_col_cycles;
             let normal_256 = remote_write.normal_jump_cycle_256;
             let normal_32 = remote_write.normal_jump_cycle_32;
@@ -75,7 +73,7 @@ fn main() -> eyre::Result<()> {
                 jump_one_cycle, jump_multiple_cycle, total_jumps_all, total_jumps_covered_by_row_open, jumps_not_covered_when_no_row_open, jumps_not_covered_when_more_shift
             );
 
-            let my_jump256: MyJumpOpt<4, 256> = remote_write.my_jump_opt_4_256;
+            let my_jump256 = remote_write.my_jump_opt_4_256;
             let MyJumpOpt {
                 multi_jump_cycle,
                 one_jump_cycle,
@@ -96,8 +94,23 @@ fn main() -> eyre::Result<()> {
                 global_row_cycles,
             } = my_jump256;
             println!(
-                "my_jump256: multi_jump_cycle: {:?} one_jump_cycle: {:?} opt_saved_times: {:?} opt_saved_cycles: {:?} all_cycle_hist_0: {:?} all_cycle_hist_1_2: {:?} all_cycle_hist_3_4: {:?} all_cycle_hist_5_8: {:?} all_cycle_hist_9_and_more: {:?}",
-                multi_jump_cycle, one_jump_cycle, opt_saved_times, opt_saved_cycles, all_cycle_hist_0, all_cycle_hist_1_2, all_cycle_hist_3_4, all_cycle_hist_5_8, all_cycle_hist_9_and_more
+                "my_jump256: multi_jump_cycle: {multi_jump_cycle:?} \
+                 one_jump_cycle: {one_jump_cycle:?} \
+                 opt_saved_times: {opt_saved_times:?} \
+                 opt_saved_cycles: {opt_saved_cycles:?} \
+                 all_cycle_hist_0: {all_cycle_hist_0:?} \
+                 all_cycle_hist_1_2: {all_cycle_hist_1_2:?} \
+                 all_cycle_hist_3_4: {all_cycle_hist_3_4:?} \
+                 all_cycle_hist_5_8: {all_cycle_hist_5_8:?} \
+                 all_cycle_hist_9_and_more: {all_cycle_hist_9_and_more:?} \
+                 row_cycle_total: {row_cycle_total:?} \
+                 total_accesses: {total_accesses:?} \
+                 row_hits: {row_hits:?} \
+                 row_misses: {row_misses:?} \
+                 gloabl_row_accesses: {gloabl_row_accesses:?} \
+                 global_row_hits: {global_row_hits:?} \
+                 global_row_miss: {global_row_miss:?} \
+                 global_row_cycles: {global_row_cycles:?}"
             );
             let my_jump_32 = remote_write.my_jump_opt_4_32;
             let MyJumpOpt {
@@ -120,8 +133,23 @@ fn main() -> eyre::Result<()> {
                 global_row_cycles,
             } = my_jump_32;
             println!(
-                "my_jump_32 multi_jump_cycle: {:?} one_jump_cycle: {:?} opt_saved_times: {:?} opt_saved_cycles: {:?} all_cycle_hist_0: {:?} all_cycle_hist_1_2: {:?} all_cycle_hist_3_4: {:?} all_cycle_hist_5_8: {:?} all_cycle_hist_9_and_more: {:?}",
-                multi_jump_cycle, one_jump_cycle, opt_saved_times, opt_saved_cycles, all_cycle_hist_0, all_cycle_hist_1_2, all_cycle_hist_3_4, all_cycle_hist_5_8, all_cycle_hist_9_and_more
+                "my_jump256: multi_jump_cycle: {multi_jump_cycle:?} \
+                 one_jump_cycle: {one_jump_cycle:?} \
+                 opt_saved_times: {opt_saved_times:?} \
+                 opt_saved_cycles: {opt_saved_cycles:?} \
+                 all_cycle_hist_0: {all_cycle_hist_0:?} \
+                 all_cycle_hist_1_2: {all_cycle_hist_1_2:?} \
+                 all_cycle_hist_3_4: {all_cycle_hist_3_4:?} \
+                 all_cycle_hist_5_8: {all_cycle_hist_5_8:?} \
+                 all_cycle_hist_9_and_more: {all_cycle_hist_9_and_more:?} \
+                 row_cycle_total: {row_cycle_total:?} \
+                 total_accesses: {total_accesses:?} \
+                 row_hits: {row_hits:?} \
+                 row_misses: {row_misses:?} \
+                 gloabl_row_accesses: {gloabl_row_accesses:?} \
+                 global_row_hits: {global_row_hits:?} \
+                 global_row_miss: {global_row_miss:?} \
+                 global_row_cycles: {global_row_cycles:?}"
             );
 
             println!("\n\n");
@@ -155,12 +183,23 @@ fn print_fn<'a>(
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::collections::BTreeMap;
+
     #[test]
     fn test() {
         let my_map: BTreeMap<_, _> = [("hello".to_string(), 1), ("world".to_string(), 2)]
             .into_iter()
             .collect();
         println!("{:?}", my_map.get("hello"));
+    }
+
+    #[test]
+    fn test_print() {
+        let a = "hello
+        world";
+        let b = "hello\n\
+        world";
+        println!("{}", a);
+        println!("{}", b);
     }
 }
